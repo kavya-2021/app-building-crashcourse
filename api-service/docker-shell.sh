8dd2b3edfc42c6f5b7169abf4cdd0152961fdd5d
SiddarthaRachakonda <153978118+SiddarthaRachakonda@users.noreply.github.com>
2024-06-12 08:15:16 +0530

    whisper install

--- api-service/docker-shell.sh
+++ api-service/docker-shell.sh
@@ -14,4 +14,4 @@ docker build -t $IMAGE_NAME -f Dockerfile .
 docker run --rm --name $IMAGE_NAME -ti \
 --mount type=bind,source="$BASE_DIR",target=/app \
 -p 9000:9000 \
--e DEV=1 $IMAGE_NAME
+-e DEV=2 $IMAGE_NAME
